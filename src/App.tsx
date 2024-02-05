12d387ca39186213d67e63b1c8b4bac26bd20918
skiran017 <skiran017@gmail.com>
2024-02-06 01:21:09 +0530

    feat: add assets, css styling and basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Home from './components/Home';
+
 function App() {
-  return <>APP</>;
+  return (
+    <Box width="400px">
+      Navbar
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
